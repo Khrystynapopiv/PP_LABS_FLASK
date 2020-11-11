24e468f12fec5ebe63d55218f886af3738b584fa
Khrystynapopiv <56650503+Khrystynapopiv@users.noreply.github.com>
2020-11-11 10:44:46 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from wsgiref.simple_server import make_server
+
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-22')
+def hello():
+    return 'Hello World 22'
+
+
+server = make_server('', 8000, app)
+print('http://127.0.0.1:8000/api/v1/hello-world-22')
+server.serve_forever()
